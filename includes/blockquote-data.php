ad40ecfd58e07d91cd84de93fd3b90e6cb6a08f5
Valentino Pesce <valentino@iltuobrand.it>
2020-03-22 17:37:05 +0100

    Aggiorna info dataset

--- includes/blockquote-data.php
+++ includes/blockquote-data.php
@@ -15,7 +15,7 @@
         
         <blockquote class="blockquote mt-5 mb-4">
             <p class="mb-0"></p>
-            <footer class="blockquote-footer">Editore/Autore del dataset: Dipartimento della Protezione Civile <strong>Elaborazione e gestione dati a cura del Dipartimento della Protezione Civile</strong></footer>
+            <footer class="blockquote-footer">Editore/Autore del dataset: dati forniti dal Ministero della Salute <strong>Elaborazione e gestione dati a cura del Dipartimento della Protezione Civile</strong></footer>
             <div>
                 <small class="text-muted">Questo sito Web declina qualsiasi dichiarazione e garanzia in merito ai dati mostrati, inclusi accuratezza, idoneità all'uso e commerciabilità. </small> <br>
                 <small class="text-light">Questo non è un sito istituzionale ma solo a scopo informativo. Rivolgiti all'autorità sanitaria locale per consigli.</small>
