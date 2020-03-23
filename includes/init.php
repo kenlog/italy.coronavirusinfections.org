edd98a8da056384c62d5057d465d7cabbfb38017
Valentino Pesce <valentino@iltuobrand.it>
2020-03-23 18:53:02 +0100

    Aggiungi report 23 Marzo 2020

--- includes/init.php
+++ includes/init.php
@@ -12,9 +12,9 @@
 
 $host = "https://italy.coronavirusinfections.org";
 
-$updateDate = '22 Marzo 2020';
+$updateDate = '23 Marzo 2020';
 
-$nextUpdate = '6:30 PM CET 23 Marzo 2020'; 
+$nextUpdate = '6:30 PM CET 24 Marzo 2020'; 
 
 $pathReportFolder = 'data/csv/reports/';
 
