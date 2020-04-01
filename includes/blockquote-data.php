a6f75ad3dfb6b2b5de7ea90de4fe9bef4b9d5dbe
Valentino Pesce <valentino@iltuobrand.it>
2020-04-01 18:29:36 +0200

    Aggiorna info dataset

--- includes/blockquote-data.php
+++ includes/blockquote-data.php
@@ -15,9 +15,9 @@
         
         <blockquote class="blockquote mt-5 mb-4">
             <p class="mb-0"></p>
-            <footer class="blockquote-footer">Editore/Autore del dataset: dati forniti dal Ministero della Salute <strong>Elaborazione e gestione dati a cura del Dipartimento della Protezione Civile</strong></footer>
+            <footer class="blockquote-footer">Editore/Autore del dataset: <a href="https://github.com/pcm-dpc/COVID-19">Elaborazione e gestione dati a cura del Dipartimento della Protezione Civile.</a> Dati forniti dal Ministero della Salute.</footer>
             <div>
-                <small class="text-muted">Questo sito Web declina qualsiasi dichiarazione e garanzia in merito ai dati mostrati, inclusi accuratezza, idoneità all'uso e commerciabilità. </small> <br>
+                <small class="text-muted">Questo sito web declina qualsiasi dichiarazione e garanzia in merito ai dati mostrati, inclusi accuratezza, idoneità all'uso e commerciabilità. </small> <br>
                 <small class="text-light">Questo non è un sito istituzionale ma solo a scopo informativo. Rivolgiti all'autorità sanitaria locale per consigli.</small>
             </div>
         </blockquote>
