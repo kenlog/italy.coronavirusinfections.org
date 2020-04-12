e63648d802dcc7c6e9182570add39c3315994af0
Valentino Pesce <valentino@iltuobrand.it>
2020-04-12 21:06:03 +0200

    Aggiorna deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo ">>> Deploy in corso >>>"
-git ftp push --user infct19ita --passwd V@3oau35 --insecure ftpes://italy.coronavirusinfections.org/
+git ftp push --user infct19ita --passwd 3jJ5b5@w --insecure ftpes://italy.coronavirusinfections.org/
