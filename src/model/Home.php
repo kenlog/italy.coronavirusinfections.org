4497eadd0c97dde9f895ffba3d42f84ae55f63fa
Valentino Pesce <valentino@iltuobrand.it>
2020-04-30 18:19:56 +0200

    Aggiungi report 30 Aprile 2020

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '29 Aprile 2020';
-	private $nextUpdate = '6:30 PM CET 30 Aprile 2020';
+	private $updateDate = '30 Aprile 2020';
+	private $nextUpdate = '6:30 PM CET 1 Maggio 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
