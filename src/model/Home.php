4e6837472e395781c52a69be9789a0808eefc981
Valentino Pesce <valentino@iltuobrand.it>
2020-05-28 18:41:29 +0200

    Aggiungi report 28 Maggio 2020

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '27 Maggio 2020';
-	private $nextUpdate = '6:30 PM CET 28 Maggio 2020';
+	private $updateDate = '28 Maggio 2020';
+	private $nextUpdate = '6:30 PM CET 29 Maggio 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
