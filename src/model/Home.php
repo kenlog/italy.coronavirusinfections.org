34cf43d3a0e609a615ca6e10c966af450a5c1ef5
Valentino Pesce <valentino@iltuobrand.it>
2020-06-01 19:02:01 +0200

    Aggiungi report 1 Giugno 2020

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '31 Maggio 2020';
-	private $nextUpdate = '6:30 PM CET 1 Giugno 2020';
+	private $updateDate = '1 Giugno 2020';
+	private $nextUpdate = '6:30 PM CET 2 Giugno 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
