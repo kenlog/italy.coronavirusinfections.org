39daf417c58b16c046d8e588466058570c550642
Valentino Pesce <valentino@iltuobrand.it>
2020-06-03 18:32:17 +0200

    Aggiungi report 3 Giugno 2020

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '2 Giugno 2020';
-	private $nextUpdate = '6:30 PM CET 3 Giugno 2020';
+	private $updateDate = '3 Giugno 2020';
+	private $nextUpdate = '6:30 PM CET 4 Giugno 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
