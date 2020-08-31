6b664cafe1ececd5680e05665e6807607122f0d3
Valentino Pesce <valentino@iltuobrand.it>
2020-08-31 20:20:23 +0200

    Report 31 Agosto 2020

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '30 Agosto 2020';
-	private $nextUpdate = '31 Agosto 2020';
+	private $updateDate = '31 Agosto 2020';
+	private $nextUpdate = '1 Settembre 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
