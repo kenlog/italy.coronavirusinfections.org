25a41c9fe6fcec07a7b6568458cf85e17b3be946
Valentino Pesce <valentino@iltuobrand.it>
2020-09-18 20:25:19 +0200

    Report 18 Settembre 2020

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '17 Settembre 2020';
-	private $nextUpdate = '18 Settembre 2020';
+	private $updateDate = '18 Settembre 2020';
+	private $nextUpdate = '19 Settembre 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
