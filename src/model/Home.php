d2cac07989e81f38f2f0f82a3ce50daa47bab22a
Valentino Pesce <valentino@iltuobrand.it>
2020-10-12 20:56:41 +0200

    Aggiornamento dati

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '10 Ottobre 2020';
-	private $nextUpdate = '12 Ottobre 2020';
+	private $updateDate = '12 Ottobre 2020';
+	private $nextUpdate = '13 Ottobre 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
