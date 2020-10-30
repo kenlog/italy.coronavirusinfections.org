c129026f09819a20bdaef5350e138321d00f166f
Kenlog <valentino@iltuobrand.it>
2020-10-30 19:30:37 +0100

    Report 30 Ottobre 2020

--- src/model/Home.php
+++ src/model/Home.php
@@ -14,8 +14,8 @@ namespace Coronavirusinfections\Model;
 
 class Home
 {
-	private $updateDate = '29 Ottobre 2020';
-	private $nextUpdate = '30 Ottobre 2020';
+	private $updateDate = '30 Ottobre 2020';
+	private $nextUpdate = '31 Ottobre 2020';
 	private $pathReportFolder;
 	private $reportFolder;
 	private $contagionDays;
